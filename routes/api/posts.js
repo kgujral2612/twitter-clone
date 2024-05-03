a02e3ef9d8fff98f40ed3c84790733db9cbfa4e1
kaushambi.gujral <kaushambi.gujral@eroad.com>
2024-05-03 16:41:13 -0700

    populate newsfeed with all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,7 +8,13 @@ const Post = require('../../schemas/PostSchema');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", (req, res, next) => {
-    
+    Post.find()
+    .populate("postedBy")
+    .then(results => res.status(200).send(results))
+    .catch(err=> {
+        console.log(`Error while fetching all posts ${err}`);
+        res.status(400);
+    })
 })
 
 router.post("/", async (req, res, next) => {
