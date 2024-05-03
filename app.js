900fc5c831f1ef7f4aea5a3921cd82a827532fa5
kaushambi.gujral <kaushambi.gujral@eroad.com>
2024-05-02 19:48:33 -0700

    added login page & bootstrap

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const middleware = require('./middleware');
 
 const server = app.listen(port, ()=>{
     console.log("Server listening on port " + port);
@@ -9,11 +10,14 @@ const server = app.listen(port, ()=>{
 app.set("view engine", "pug"); //telling the server that we're using pug as the template engine
 app.set("views", "views"); //all views will be in the views folder
 
-app.get("/", (req, res, next)=> { 
+// Routes
+const loginRoute = require('./routes/loginRoutes');
 
+app.use("/login", loginRoute);
+
+app.get("/", middleware.requireLogin, (req, res, next)=> { 
     var payload = {
         pageTitle : "Home"
     }
-
     res.status(200).render("home", payload);
 })
