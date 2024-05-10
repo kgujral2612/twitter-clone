92a2ab03248392cd7dc3fb1b5561cca78b529335
kaushambi.gujral <kaushambi.gujral@eroad.com>
2024-05-09 20:05:14 -0700

    retrieve chat data from db and store in js

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
-const bodyParser = require("body-parser")
-const bcrypt = require("bcrypt");
+const Chat = require('../schemas/ChatSchema');
 const User = require('../schemas/UserSchema');
 
 router.get("/", (req, res, next) => {
@@ -20,5 +18,41 @@ router.get("/new", (req, res, next) => {
         userLoggedInJs: JSON.stringify(req.session.user)
     });
 })
+
+router.get("/:chatId", async (req, res, next) => {
+    var userId = req.session.user._id;
+    var chatId = req.params.chatId;
+
+    var payload = {
+        pageTitle: "Chat",
+        userLoggedIn: req.session.user,
+        userLoggedInJs: JSON.stringify(req.session.user)
+    }
+
+    console.log(payload);
+    var chat = await Chat.findOne({_id: chatId, users: { $elemMatch : { $eq: userId} }})
+               .populate("users");
+    
+    console.log(chat);
+
+    if(chat == null){
+        //check if chat id is user id
+        var userFound = await User.findById(chatId);
+
+        if(userFound == null){
+            //get chat using user id
+        }
+    }
+    
+    if(chat == null){
+        payload.errorMessage = "Chat does not exist or you do not have permission to view it";
+    }
+    else{
+        payload.chat = chat;
+    }
+
+    res.status(200).render("chatPage", payload);
+})
+
 
 module.exports = router;
